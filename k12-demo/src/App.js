8986b64150d36a263a32a06008b422d9c5e69329
mohit <mohitdhawan92@gmil.com>
2022-04-01 14:06:28 +0530

    first comment

---
+++ k12-demo/src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+function App() {
+  return (
+    <div className="App" id="root">
+     <p>I am learning React. My life is getting better.</p>
+    </div>
+  );
+}
+
+export default App;
